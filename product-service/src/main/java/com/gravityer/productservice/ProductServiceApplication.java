4caaf2bfd178df50f09eb7513ced253b2289675b
Syed Sadiqu Hussain <syed@gravityer.com>
2025-09-18 13:31:33 +0530

    register with eureka + basic products setup

---
+++ product-service/src/main/java/com/gravityer/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gravityer.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+}
---
+++ product-service/src/main/java/com/gravityer/productservice/model/Product.java
@@ -0,0 +1,20 @@
+package com.gravityer.productservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+}
