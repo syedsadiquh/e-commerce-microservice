d11345a5b1f25dbc0a94de7bfd91b2770404c7b3
Syed Sadiqu Hussain <syed@gravityer.com>
2025-09-25 15:25:49 +0530

    rename customer-service to user-service

--- customer-service/src/main/java/com/gravityer/customerservice/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gravityer.customerservice.repositories;
-
-import com.gravityer.customerservice.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- user-service/src/main/java/com/gravityer/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/gravityer/userservice/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerServiceApplication {
+public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CustomerServiceApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
 }
